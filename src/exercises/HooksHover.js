2b3914566dfc3666415214633992be26d943d589
opauloh <paulo.scape@gmail.com>
2020-05-23 23:50:08 -0300

    Hooks Hover with customHooks

--- src/exercises/HooksHover.js
+++ src/exercises/HooksHover.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 
 
+function useHover() {
+    const [hovering, setHovering] = useState(false);
+    const mouseOver = () => setHovering(true);
+    const mouseOut = () => setHovering(false);
+
+    const attrs = {
+        onMouseOver: mouseOver,
+        onMouseOut: mouseOut
+    }
+
+    return [hovering, attrs];
+}
+
 function Hover({ children }) {
     const [hovering, setHovering] = useState(false);
 
@@ -15,25 +28,32 @@ function Hover({ children }) {
 }
 
 export default function HooksHover() {
+    const [hovering, attrs] = useHover();
 
     return (
         <>
+            Render Props (Children)
             <Hover>
                 {(hovering) => <div style={{ height: 100, background: 'cyan' }}>
                     Is Hovering? {hovering ? "yes" : "no"}
                 </div>}
             </Hover>
             <hr />
+            Render Props
             <HoverProps render={(hovering) => (
                 <div style={{ height: 100, background: 'cyan' }}>
                     Is Hovering? {hovering ? "yes" : "no"}
                 </div>
             )}
             />
+            <hr />
+            Custom Hooks (useHover)
+            <div style={{ height: 100, background: 'cyan' }} {...attrs} >
+                Is Hovering? {hovering ? "yes" : "no"}
+            </div>
         </>
     );
 }
-
 
 
 function HoverProps({ render }) {
